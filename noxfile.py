ed636ab565e5c853004803828020525e293b61e1
Michael <35783820+mib1185@users.noreply.github.com>
2021-09-13 00:02:21 +0200

    cleanup dependencies (#123)

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,4 @@
 """Nox sessions."""
-import shutil
 import sys
 from pathlib import Path
 from textwrap import dedent
@@ -20,7 +19,6 @@ nox.options.sessions = (
     "tests",
     # "typeguard",
     # "xdoctest",
-    # "docs-build",
 )
 
 
@@ -158,31 +156,3 @@ def xdoctest(session: Session) -> None:
     session.install(".")
     session.install("xdoctest[colors]")
     session.run("python", "-m", "xdoctest", package, *args)
-
-
-@nox.session(name="docs-build", python="3.8")
-def docs_build(session: Session) -> None:
-    """Build the documentation."""
-    args = session.posargs or ["docs", "docs/_build"]
-    session.install(".")
-    session.install("sphinx", "sphinx-rtd-theme")
-
-    build_dir = Path("docs", "_build")
-    if build_dir.exists():
-        shutil.rmtree(build_dir)
-
-    session.run("sphinx-build", *args)
-
-
-@nox.session(python="3.8")
-def docs(session: Session) -> None:
-    """Build and serve the documentation with live reloading on file changes."""
-    args = session.posargs or ["--open-browser", "docs", "docs/_build"]
-    session.install(".")
-    session.install("sphinx", "sphinx-autobuild", "sphinx-rtd-theme")
-
-    build_dir = Path("docs", "_build")
-    if build_dir.exists():
-        shutil.rmtree(build_dir)
-
-    session.run("sphinx-autobuild", *args)
